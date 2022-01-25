e469cc63fe1d303227fc5eed39e17a13967aa5db
unknown <anastasiia.paniotova@nure.ua>
2022-01-25 22:01:15 +0300

    added Asserts to tests

--- TestNG/src/test/java/DouTests.java
+++ TestNG/src/test/java/DouTests.java
@@ -8,10 +8,13 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
 
 import java.time.Duration;
 
@@ -38,7 +41,9 @@ public class DouTests {
         Actions moveMouse = new Actions(driver);
         moveMouse.moveToElement(forumElement).perform();
 
-        System.out.println(forumElement.getCssValue("color"));
+        String actual = forumElement.getCssValue("color");
+
+        Assert.assertEquals(actual,"rgba(255, 0, 0, 1)", "Error: color is not red");
     }
 
     //клик на лого редиректит юзера на главную страницу
@@ -53,7 +58,8 @@ public class DouTests {
         logo.click();
 
         String URL = driver.getCurrentUrl();
-        System.out.println(URL);
+
+        assertThat(URL,equalTo("https://dou.ua/"));
 
     }
 
@@ -66,8 +72,9 @@ public class DouTests {
         WebElement author = driver.findElement(By.className("name"));
         WebElement title = driver.findElement(By.xpath("//*[@class='b-typo b-typo_post']/h1"));
 
-        System.out.println("Автор: " + author.getText());
-        System.out.println("Название статьи: " + title.getText());
+        //возможно они зафейляться, так как статьи меняются иногда
+        assertThat(author.getText(), equalToIgnoringCase("Taras Ustyianovych, Operational Intelligence Engineer в EPAM"));
+        assertThat(title.getText(),equalToIgnoringCase("MLOps: універсальний гайд з моніторингу моделей на проді"));
 
     }
 
@@ -81,8 +88,10 @@ public class DouTests {
         WebElement result = driver.findElement(By.xpath("//*[@class='gs-bidi-start-align gs-snippet'][1]"));
 
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+        Boolean actualResult = result.getText().contains(data);
 
-        System.out.println(result.getText().contains(data));
+        Assert.assertTrue(actualResult,"Text doesn't contain keyword");
+
     }
 
     //проверка что есть страница "Новий топік"
@@ -95,7 +104,9 @@ public class DouTests {
                 .until(ExpectedConditions.elementToBeClickable(By.xpath("//div[@class='wrap add-content-link']/a"))));
         addTopic.click();
 
-        System.out.println(driver.findElement(By.className("page-head")).getText().contains("Новий топік"));
+        Boolean actualResult = driver.findElement(By.className("page-head")).getText().contains("Новий топік");
+
+        Assert.assertTrue(actualResult,"Wrong title");
 
     }
 
